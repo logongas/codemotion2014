0bc91a6496b6bb3fc395b1416730b211f9df638e
logongas <lorenzo.profesor@gmail.com>
2014-11-12 13:24:39 +0100

    organizar el codigo

--- public_html/link/04-prelink.js
+++ public_html/link/04-prelink.js
@@ -6,11 +6,13 @@ app.directive("caOptions", [function () {
         compile: function (tElement, tAttrs) {
             return {
                 pre: function (scope, iElement, iAttrs, controller, transcludeFn) {
-                    var isArrayObjects = scope.$eval(" metadata['" + iAttrs.ngModel + "'].isArrayObjects");
+                    var model=iAttrs.ngModel;
+                    var isArrayObjects = scope.$eval(" metadata['" + model + "'].isArrayObjects");
+
                     if (isArrayObjects === true) {
-                        iAttrs.ngOptions = "value as value.toString for value in metadata['" + tAttrs.ngModel + "'].values";
+                        iAttrs.ngOptions = "value as value.toString for value in metadata['" + model + "'].values";
                     } else {
-                        iAttrs.ngOptions = "value for value in metadata['" + tAttrs.ngModel + "'].values";
+                        iAttrs.ngOptions = "value for value in metadata['" + model + "'].values";
                     }
                 },
                 post: function (scope, iElement, iAttrs, controller, transcludeFn) {
@@ -23,26 +25,26 @@ app.directive("caOptions", [function () {
 
 app.controller("PruebaController", ['$scope', function ($scope) {
 
-        $scope.metadata={};
-        
-        $scope.metadata.ciudad = {
-            isArrayObjects: true,
-            values: [
-                {toString: "Madrid", pais: "España"},
-                {toString: "Berlin", pais: "Alemania"},
-                {toString: "Tel Aviv", pais: "Israel"},
-                {toString: "Milan", pais: "Italia"},                
-            ]
-        };
+    $scope.metadata={};
 
-        $scope.metadata.diaSemana = {
-            isArrayObjects: false,
-            values: [
-                "Lunes","Martes","Miercoles","Jueves","Sabado","Domingo"
-            ]
-        };
+    $scope.metadata.ciudad = {
+        isArrayObjects: true,
+        values: [
+            {toString: "Madrid", pais: "España"},
+            {toString: "Berlin", pais: "Alemania"},
+            {toString: "Tel Aviv", pais: "Israel"},
+            {toString: "Milan", pais: "Italia"},                
+        ]
+    };
 
-        $scope.ciudad;
-        $scope.diaSemana;
+    $scope.metadata.diaSemana = {
+        isArrayObjects: false,
+        values: [
+            "Lunes","Martes","Miercoles","Jueves","Sabado","Domingo"
+        ]
+    };
+
+    $scope.ciudad;
+    $scope.diaSemana;
         
-    }]);
+}]);
